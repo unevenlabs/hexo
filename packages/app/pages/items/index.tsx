62d71ebeed0819251fbbd5511235545e449068f7
Peter Watts <peter@peters-mbp.lan>
2021-08-09 13:21:22 +1000

    preview all

--- packages/app/pages/items/index.tsx
+++ packages/app/pages/items/index.tsx
@@ -12,6 +12,15 @@ const WithStaticProps = ({ items }: Props) => (
     {items.map((item) => (
         <span>
         <img title={item} width="150" src={`/images/black/${item}.svg`} />
+        <img title={item} width="150" src={`/images/blue/${item}.svg`} />
+        <img title={item} width="150" src={`/images/red/${item}.svg`} />
+        <img title={item} width="150" src={`/images/purple/${item}.svg`} />
+        <img title={item} width="150" src={`/images/yellow/${item}.svg`} />
+        <img title={item} width="150" src={`/images/orange/${item}.svg`} />
+        <img title={item} width="150" src={`/images/green/${item}.svg`} />
+        <img title={item} width="150" src={`/images/gray/${item}.svg`} />
+        <img title={item} width="150" src={`/images/brown/${item}.svg`} />
+        <img title={item} width="150" src={`/images/pink/${item}.svg`} />
         <img title={item} width="150" src={`/images/white/${item}.svg`} />
         </span>
       ))}
