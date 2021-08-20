954656e035670f3d80d581d72e14968166b30e63
George Roman <george.roman.99@gmail.com>
2021-08-20 14:42:55 +0200

    Add missing dependency to useEffect

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Index() {
       }
       setMintedItems(localMintedItems);
     }
-  }, []);
+  }, [mintedItemsInfo]);
 
   // Filters for items
   const [showAll, setShowAll] = useState(true);
@@ -81,28 +81,31 @@ export default function Index() {
                       alt=""
                     />
                   </a>
-                  <Popover.Group as="nav" className="hidden md:flex space-x-10 pl-10 pt-2">
-                  <a
-                    href="#browse"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  <Popover.Group
+                    as="nav"
+                    className="hidden md:flex space-x-10 pl-10 pt-2"
                   >
-                    Browse Hexos
-                  </a>
-                  <a
-                    href="https://discord.gg/XveChR6bsE"
-                    target="_blank"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Discord
-                  </a>
-                  <a
-                    href="https://github.com/unevenlabs/hexo"
-                    target="_blank"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Github
-                  </a>
-                </Popover.Group>
+                    <a
+                      href="#browse"
+                      className="text-base font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      Browse Hexos
+                    </a>
+                    <a
+                      href="https://discord.gg/XveChR6bsE"
+                      target="_blank"
+                      className="text-base font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      Discord
+                    </a>
+                    <a
+                      href="https://github.com/unevenlabs/hexo"
+                      target="_blank"
+                      className="text-base font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      Github
+                    </a>
+                  </Popover.Group>
                 </div>
                 <div className="-mr-2 -my-2 md:hidden">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -110,7 +113,6 @@ export default function Index() {
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
-                
 
                 <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                   {active ? (
@@ -169,45 +171,47 @@ export default function Index() {
                   </div>
                   <div className="py-6 px-5 space-y-6">
                     <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <a
-                    href="#browse"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Browse Hexos
-                  </a>
-                  <a
-                    href="https://discord.gg/XveChR6bsE"
-                    target="_blank"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Discord
-                  </a>
-                  <a
-                    href="https://github.com/unevenlabs/hexo"
-                    target="_blank"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Github
-                  </a>
+                      <a
+                        href="#browse"
+                        className="text-base font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Browse Hexos
+                      </a>
+                      <a
+                        href="https://discord.gg/XveChR6bsE"
+                        target="_blank"
+                        className="text-base font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Discord
+                      </a>
+                      <a
+                        href="https://github.com/unevenlabs/hexo"
+                        target="_blank"
+                        className="text-base font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Github
+                      </a>
                     </div>
                     <div>
-                    {active ? (
-                    <>
-                      <span>
-                        ({chainId === 1 ? "Mainnet" : "Rinkeby"}){" "}
-                        {account.slice(0, 6) + "..." + account.slice(-4, -1)}
-                      </span>
-                    </>
-                  ) : (
-                    <button
-                      className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                      onClick={() =>
-                        activateConnector(web3ReactContext, injected)
-                      }
-                    >
-                      Connect Wallet
-                    </button>
-                  )}
+                      {active ? (
+                        <>
+                          <span>
+                            ({chainId === 1 ? "Mainnet" : "Rinkeby"}){" "}
+                            {account.slice(0, 6) +
+                              "..." +
+                              account.slice(-4, -1)}
+                          </span>
+                        </>
+                      ) : (
+                        <button
+                          className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                          onClick={() =>
+                            activateConnector(web3ReactContext, injected)
+                          }
+                        >
+                          Connect Wallet
+                        </button>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -309,9 +313,7 @@ export default function Index() {
             </div>
           </div>
           <div className="max-w-3xl mx-auto text-center">
-            <p className="mt-10 text-xl text-gray-500">
-              Hi, I'm Red Dragon
-            </p>
+            <p className="mt-10 text-xl text-gray-500">Hi, I'm Red Dragon</p>
           </div>
         </div>
       </div>
