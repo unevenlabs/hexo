16957a29c6ba7b9834ef3c0918f341f3bb32f17b
Peter Watts <pwattsmail@gmail.com>
2021-08-21 16:08:31 +1000

    add 20 random options

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -481,11 +481,28 @@ export default function Index() {
                 id="random"
                 name="random"
                 className="float-right pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-l-md"
-                defaultValue="1 Random"
+                defaultValue="1"
               >
-                <option>1 random</option>
-                <option>2 random</option>
-                <option>3 random</option>
+                <option value="1">1 random</option>
+                <option value="2">2 random</option>
+                <option value="3">3 random</option>
+                <option value="4">4 random</option>
+                <option value="5">5 random</option>
+                <option value="6">6 random</option>
+                <option value="7">7 random</option>
+                <option value="8">8 random</option>
+                <option value="9">9 random</option>
+                <option value="10">10 random</option>
+                <option value="11">11 random</option>
+                <option value="12">12 random</option>
+                <option value="13">13 random</option>
+                <option value="14">14 random</option>
+                <option value="15">15 random</option>
+                <option value="16">16 random</option>
+                <option value="17">17 random</option>
+                <option value="18">18 random</option>
+                <option value="19">19 random</option>
+                <option value="20">20 random</option>
               </select>
             </div>
           </div>
