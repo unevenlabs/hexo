d418dc7e93a6313dd4a16b58081b9f426386ffce
Diego Ramos <RamosDiego@pm.me>
2021-08-31 00:54:04 -0400

    fixed tailwindcss purge issue

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,15 +1,12 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
@@ -17,7 +14,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms"),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
   ],
-};
+}
