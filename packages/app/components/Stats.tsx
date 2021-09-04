8b05fee0db2174c3a9c2680e4c4d696a39b32de2
Peter Watts <pwattsmail@gmail.com>
2021-09-04 16:00:35 +1000

    tweak

--- packages/app/components/Stats.tsx
+++ packages/app/components/Stats.tsx
@@ -49,14 +49,6 @@ const Stats = () => {
                   </a>
                 </dd>
               </div>
-              <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                  ETH
-                </dt>
-                <dd className="order-1 text-5xl font-extrabold text-indigo-600">
-                  0.01
-                </dd>
-              </div>
             </dl>
           </div>
       <div className="text-center text-2xl text-gray-900 mt-16">
