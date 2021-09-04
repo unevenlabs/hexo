5d0d76ab0f69a39f65e110d740ff2ec250bca78f
Peter Watts <pwattsmail@gmail.com>
2021-09-04 16:03:05 +1000

    tweak

--- packages/app/components/Stats.tsx
+++ packages/app/components/Stats.tsx
@@ -14,7 +14,7 @@ const Stats = () => {
         <div className="absolute inset-0 h-1/2 bg-gray-50" />
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-5xl mx-auto">
-            <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-5">
+            <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-4">
               <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                 <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
                   Colors
